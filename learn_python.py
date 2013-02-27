9a328245f5454cd1a1d795ffc23853cd4496e779
uestcmy <mengyangmy@gmail.com>
2013-02-27 17:08:45 +0800

    learning python

---
+++ learn_python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#filename:learn_python.y
+
+from sys import *
+
+print "The command line arguements are: "
+for i in argv:
+    print i
+print "\n\nThe python path is : ",sys.path,"\n"
