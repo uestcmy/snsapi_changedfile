9294988058495bad5569855b968521514797ef23
uestcmy <mengyangmy@gmail.com>
2013-03-08 14:44:16 +0800

    learning : os module, change the file name

--- learn_python.py
+++ learn_python.py
@@ -2,9 +2,25 @@
 #filename:learn_python.y
 
 from sys import *
+import os
+import time
 
-print __name__
-print "The command line arguements are: "
-for i in argv:
-    print i
-print "\n\nThe python path is : ",path,"\n"
+
+print __name__ #show whether the module is called
+
+class myfirstclass:
+    """build a class a object
+    """
+print(myfirstclass.__doc__) #print the docstring of class
+o = myfirstclass()
+
+import os
+print "OS name = ",os.name
+
+for key in os.environ:
+    print "key = %s \t\t\t\t value = %s "% (key,os.environ[key])
+
+#os.rename("./test","./test") 
+    #change the dir name
+#os.rename("./test1/index.rst","./test1/index-1.rst")
+    # change the file name
